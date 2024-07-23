0775a600d257bd756b369ad01dba798b86a2f454
manish <ms569624@gmail.com>
2024-07-24 01:14:09 +0530

    new changes

--- Socket/socket.js
+++ Socket/socket.js
@@ -1,6 +1,145 @@
 var axios = require("axios");
 const socketIo = require("socket.io");
 
+const scorecboard = {
+    "message": "Match score fetched.",
+    "code": 0,
+    "error": false,
+    "data": {
+        "rky": "cricnet_33418377",
+        "match_id": "33418377",
+        "match_name": "",
+        "match_date": "",
+        "venue": "",
+        "msg": "OI Needed 24 runs from 46 balls",
+        "teams": [
+            {
+                "team_name": "OI",
+                "team_short_name": "OI",
+                "score": "66-2 (10.4)"
+            },
+            {
+                "team_name": "BP",
+                "team_short_name": "BP",
+                "score": "89-10 (16.1)"
+            }
+        ],
+        "currentRunRate": "6.11",
+        "current_inning": "OI",
+        "remaining_overs": 0,
+        "requireRunRate": "2.61",
+        "runNeeded": 23,
+        "ballsRemaining": 0,
+        "target": "89",
+        "current_over": "(10.4)",
+        "current_score": "66-2",
+        "current_wickets": "",
+        "match_format": "",
+        "currentPlayersScore": {
+            "Batsman": [
+                {
+                    "id": 0,
+                    "on_play": "*",
+                    "player_id": 0,
+                    "team_id": 0,
+                    "match_id": "",
+                    "inning": "",
+                    "runs": "0",
+                    "balls": "0",
+                    "fours": "0",
+                    "sixes": "0",
+                    "is_out": "0",
+                    "out_text": "",
+                    "strike_rate": ""
+                },
+                {
+                    "id": 0,
+                    "on_play": "",
+                    "player_id": 0,
+                    "team_id": 0,
+                    "match_id": "",
+                    "inning": "",
+                    "runs": "0",
+                    "balls": "0",
+                    "fours": "0",
+                    "sixes": "0",
+                    "is_out": "0",
+                    "out_text": "",
+                    "strike_rate": ""
+                }
+            ],
+            "partnership": "",
+            "lastWicket": "",
+            "bowler": ""
+        },
+        "last24balls": [
+            {
+                "score_card": "1",
+                "out_text": "1",
+                "comment": ""
+            },
+            {
+                "score_card": "0",
+                "out_text": "0",
+                "comment": ""
+            },
+            {
+                "score_card": "1",
+                "out_text": "1",
+                "comment": ""
+            },
+            {
+                "score_card": "0",
+                "out_text": "0",
+                "comment": ""
+            },
+            {
+                "score_card": "1",
+                "out_text": "1",
+                "comment": ""
+            },
+            {
+                "score_card": "",
+                "out_text": "",
+                "comment": ""
+            }
+        ],
+        "last24ballsNew": [
+            {
+                "score_card": "1",
+                "out_text": "1",
+                "comment": ""
+            },
+            {
+                "score_card": "0",
+                "out_text": "0",
+                "comment": ""
+            },
+            {
+                "score_card": "1",
+                "out_text": "1",
+                "comment": ""
+            },
+            {
+                "score_card": "0",
+                "out_text": "0",
+                "comment": ""
+            },
+            {
+                "score_card": "1",
+                "out_text": "1",
+                "comment": ""
+            },
+            {
+                "score_card": "",
+                "out_text": "",
+                "comment": ""
+            }
+        ],
+        "completed_message": ""
+    },
+    "iframeUrl": "https://static.stratagem.me/54411848/tracker.html?sportId=4&lang=en&liveEvent=true&providers=Betradar&eventId=49393311"
+}
 
 const updateSessionOdds = async (io, matchId, marketId) => {
   try {
@@ -44,7 +183,7 @@ const updateSessionOdds = async (io, matchId, marketId) => {
     
 
     // Emit updated data
-    io.emit("receiveData", {fetchedData,MatchOdds,scorecboardData});
+    io.emit("receiveData", {fetchedData,MatchOdds,scorecboard});
   } catch (error) {
     console.error("Error updating session odds:", error.message);
   }
