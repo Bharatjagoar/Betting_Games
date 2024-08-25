dd8094cf2564b38254c84d9a5341ce37da56f0b2
Bharat Jagoar <bharatdigitechwizard@outlook.com>
2024-08-25 06:20:41 +0530

    just like that changes

--- app/website/lottery/controller/lotteryController.js
+++ app/website/lottery/controller/lotteryController.js
@@ -14,286 +14,320 @@ const clientModel = require("../../../admin/Users/models/client.model.js")
 
 
 module.exports.createBet = async (req, res) => {
-  console.log("hello")
-  console.log(req.body)
-  let { UserId, betType, openNumber, ammount } = req.body
-  const obj = {}
-  let createamount
-  let updateamount
-  // obj[`${betType}`] = openNumber
-  try {
-    const createBet = await lotteryBetModel.create(req.body)
-    console.log(createBet)
-    let amt = ammount
-    ammount = 0 - ammount
+    console.log("hello")
+    console.log(req.body)
+    let { UserId, betType, openNumber, ammount } = req.body
+
+    const obj = {}
+    let createamount
+    let updateamount
+    // obj[`${betType}`] = openNumber
+    try {
+        const findtheuser = await clientModel.findById(UserId)
+        if(findtheuser.coins<ammount){
+            res.status(200).send({message:"Not enough coins",data:null})
+        }
+        const createBet = await lotteryBetModel.create(req.body)
+        console.log(createBet)
+        let amt = ammount
+        ammount = 0 - ammount
 
 
-    const findAndupdate = await clientModel.findByIdAndUpdate(
-      UserId,
-      { $inc: { coins: ammount, explore: amt } },
-      { new: true }
-    )
+        const findAndupdate = await clientModel.findByIdAndUpdate(
+            UserId,
+            { $inc: { coins: ammount, explore: amt } },
+            { new: true }
+        )
 
-    console.log(findAndupdate, "find and update")
-    if (!findAndupdate) {
-      createamount = await clientModel.create({ UserId: UserId })
-      console.log(createamount, "AAAAAAAAAAAAAAAAAAAAAAAAAA")
-      updateamount = await clientModel.findByIdAndUpdate(
-        createamount.id,
-        { $inc: { Ammount: ammount } }
-      )
-      console.log(updateamount, "UUUUUUUUUUUUUUUUUU")
+        console.log(findAndupdate, "find and update")
+
+
+
+        // commented because you will never create the client model here which means you will always find the user from client model
+
+
+
+        // if (!findAndupdate) {
+        //     createamount = await clientModel.create({ UserId: UserId })
+        //     console.log(createamount, "AAAAAAAAAAAAAAAAAAAAAAAAAA")
+        //     updateamount = await clientModel.findByIdAndUpdate(
+        //         createamount.id,
+        //         { $inc: { Ammount: ammount } }
+        //     )
+            // console.log(updateamount, "UUUUUUUUUUUUUUUUUU")
+        // }
+        res.send({ createBet, createamount, updateamount })
+    } catch (error) {
+        console.log(error)
+        res.send(error)
     }
-    res.send({ createBet, createamount, updateamount })
-  } catch (error) {
-    console.log(error)
-    res.send(error)
-  }
 
 }
 
 module.exports.readBetsfromType = async (req, res) => {
-  const { betType } = req.body;
-  try {
-    let pipeline;
+    const { betType } = req.body;
+    try {
+        let pipeline;
 
-    if (betType === "teen" || betType === "jodi") {
-      pipeline = [
-        { $match: { betType: betType } },
-        {
-          $addFields: {
-            lastDigit: {
-              $cond: {
-                if: { $gte: ["$Bettingnumber", 10] },
-                then: { $mod: ["$Bettingnumber", 10] },
-                else: "$Bettingnumber"
-              }
-            }
-          }
-        },
-        {
-          $group: {
-            _id: "$lastDigit",
-            count: { $sum: 1 },
-            totalAmount: { $sum: "$ammount" }
-          }
+        if (betType === "teen" || betType === "jodi") {
+            pipeline = [
+                { $match: { betType: betType } },
+                {
+                    $addFields: {
+                        lastDigit: {
+                            $cond: {
+                                if: { $gte: ["$Bettingnumber", 10] },
+                                then: { $mod: ["$Bettingnumber", 10] },
+                                else: "$Bettingnumber"
+                            }
+                        }
+                    }
+                },
+                {
+                    $group: {
+                        _id: "$lastDigit",
+                        count: { $sum: 1 },
+                        totalAmount: { $sum: "$ammount" }
+                    }
+                }
+            ];
+        } else {
+            pipeline = [
+                { $match: { betType: betType } },
+                {
+                    $group: {
+                        _id: "$Bettingnumber",
+                        count: { $sum: 1 },
+                        totalAmount: { $sum: "$ammount" }
+                    }
+                }
+            ];
         }
-      ];
-    } else {
-      pipeline = [
-        { $match: { betType: betType } },
-        {
-          $group: {
-            _id: "$Bettingnumber",
-            count: { $sum: 1 },
-            totalAmount: { $sum: "$ammount" }
-          }
-        }
-      ];
+
+        // Common stages for all bet types
+        pipeline = pipeline.concat([
+            {
+                $project: {
+                    _id: 0,
+                    Bettingnumber: "$_id",
+                    count: 1,
+                    totalAmount: 1
+                }
+            },
+            { $sort: { Bettingnumber: 1 } }
+        ]);
+
+        const result = await lotteryBetModel.aggregate(pipeline);
+
+        console.log(result);
+        res.send(result);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
     }
-
-    // Common stages for all bet types
-    pipeline = pipeline.concat([
-      {
-        $project: {
-          _id: 0,
-          Bettingnumber: "$_id",
-          count: 1,
-          totalAmount: 1
-        }
-      },
-      { $sort: { Bettingnumber: 1 } }
-    ]);
-
-    const result = await lotteryBetModel.aggregate(pipeline);
-
-    console.log(result);
-    res.send(result);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
 };
 
 
 
 // Function to update coins and explore fields based on bet results
 async function updateBetResults(winners, losers) {
-  try {
-    // Update winners: Set explore to 0 and add the amount to coins
-    for (const winner of winners) {
-      const exp = 0 - winner.ammount;
-      console.log(winner.ammount,winner.UserId)
-      const winnerUpdate = await clientModel.findByIdAndUpdate(
-        winner.UserId,
-        {
-          $inc: { coins: winner.ammount , explore: exp} // Increase coins by the winning amount
-          // $inc: { explore: exp }             // Deduct explore to 0
-        },{new:true}
-      );
-      console.log("Updated winner:", winnerUpdate);
-    }
+    try {
+        // Update winners: Set explore to 0 and add the amount to coins
+        for (const winner of winners) {
+            const exp = 0 - winner.ammount;
+            console.log(winner.ammount, winner.UserId)
+            const winnerUpdate = await clientModel.findByIdAndUpdate(
+                winner.UserId,
+                {
+                    $inc: { coins: winner.ammount, explore: exp } // Increase coins by the winning amount
+                    // $inc: { explore: exp }             // Deduct explore to 0
+                }, { new: true }
+            );
+            console.log("Updated winner:", winnerUpdate);
+        }
 
-    // Update losers: Set explore to 0
-    for (const loser of losers) {
-      const exp = 0 - loser.ammount;
-      const loserUpdate = await clientModel.findByIdAndUpdate(
-        loser.UserId,
-        {
-          $inc: { explore: exp }  // Deduct explore to 0
-        },{new:true}
-      );
-      console.log("Updated loser:", loserUpdate);
-    }
+        // Update losers: Set explore to 0
+        for (const loser of losers) {
+            const exp = 0 - loser.ammount;
+            const loserUpdate = await clientModel.findByIdAndUpdate(
+                loser.UserId,
+                {
+                    $inc: { explore: exp }  // Deduct explore to 0
+                }, { new: true }
+            );
+            console.log("Updated loser:", loserUpdate);
+        }
 
-    console.log("Bet results updated successfully.");
-  } catch (error) {
-    console.error("Error updating bet results:", error);
-    throw error;
-  }
+        console.log("Bet results updated successfully.");
+    } catch (error) {
+        console.error("Error updating bet results:", error);
+        throw error;
+    }
 }
 
 
 
 async function getBetResults(betType, winningNumber, date) {
-  // console.log("datae", date)
-  const formattedDate = moment(date).format("DD MM YYYY");
+    // console.log("datae", date)
+    const formattedDate = moment(date).format("DD MM YYYY");
 
-  const momentDate = moment(formattedDate, "DD MM YYYY");
-  // console.log(momentDate,"thisis mmentTTTTTTTTTTTTTTTTT")
-  // Create start and end of the day as Date objects
-  const startOfDay = momentDate.startOf('day').toDate(); // 00:00:00 of the day
-  const endOfDay = momentDate.endOf('day').toDate();
-  // console.log(startOfDay,"omentTTTTTTTTTTTTTTTTT")
-  // console.log(endOfDay,"omentTTTTTTTTTTTTTTTTT")
-  let querytype;
-  if (betType === "firstNumber") {
-    querytype = "single";
-  } else if (betType === "secondNumber") {
-    querytype = "jodi";
-  } else if (betType === "thirdNumber") {
-    querytype = "teen";
-  }
+    const momentDate = moment(formattedDate, "DD MM YYYY");
+    // console.log(momentDate,"thisis mmentTTTTTTTTTTTTTTTTT")
+    // Create start and end of the day as Date objects
+    const startOfDay = momentDate.startOf('day').toDate(); // 00:00:00 of the day
+    const endOfDay = momentDate.endOf('day').toDate();
+    // console.log(startOfDay,"omentTTTTTTTTTTTTTTTTT")
+    // console.log(endOfDay,"omentTTTTTTTTTTTTTTTTT")
+    let querytype;
+    if (betType === "firstNumber") {
+        querytype = "single";
+    } else if (betType === "secondNumber") {
+        querytype = "jodi";
+    } else if (betType === "thirdNumber") {
+        querytype = "teen";
+    }
 
-  try {
-    // Query for winning bets
-    const winningBets = await lotteryBetModel.find({
-      betType: querytype,
-      Bettingnumber: winningNumber,
-      createdAt: {
-        $gte: startOfDay,
-        $lte: endOfDay
-      }
-    }, { UserId: 1, ammount: 1, _id: 0 });
+    try {
+        // Query for winning bets
+        const winningBets = await lotteryBetModel.find({
+            betType: querytype,
+            Bettingnumber: winningNumber,
+            createdAt: {
+                $gte: startOfDay,
+                $lte: endOfDay
+            }
+        }, { UserId: 1, ammount: 1, _id: 0 });
 
-    // Query for losing bets (bets not on the winning number)
-    const losingBets = await lotteryBetModel.find({
-      betType: querytype,
-      Bettingnumber: { $ne: winningNumber },
-      createdAt: {
-        $gte: startOfDay,
-        $lte: endOfDay
-      }
-    }, { UserId: 1, ammount: 1, _id: 0 });
+        // Query for losing bets (bets not on the winning number)
+        const losingBets = await lotteryBetModel.find({
+            betType: querytype,
+            Bettingnumber: { $ne: winningNumber },
+            createdAt: {
+                $gte: startOfDay,
+                $lte: endOfDay
+            }
+        }, { UserId: 1, ammount: 1, _id: 0 });
 
-    return {
-      winners: winningBets,
-      losers: losingBets
-    };
-  } catch (error) {
-    console.error("Error fetching bet results:", error);
-    throw error;
-  }
+        return {
+            winners: winningBets,
+            losers: losingBets
+        };
+    } catch (error) {
+        console.error("Error fetching bet results:", error);
+        throw error;
+    }
 }
 
 
 
 module.exports.SetBettingNumber = async (req, res) => {
-  const current = new Date();
-  const formattedDate = moment(current).format("DD MM YYYY"); // Format the date
-  const day = moment(current).format("dddd");
-  // console.log(formattedDate, day);
+    const current = new Date();
+    let numberofdays=0
+    const formattedDate = moment(current).add(numberofdays,"days").format("DD MM YYYY"); // Format the date
+    const day = moment(current).add(numberofdays, "days").format("dddd");
+    console.log(formattedDate,day)
+    // console.log(formattedDate, day);
 
-  const { number, type } = req.body;
-  let obj = {};
-  obj[type] = number;
+    const { number, type } = req.body;
+    let obj = {};
+    obj[type] = number;
 
-  try {
-    // Find the Lottery Document by date
-    // const findDOC = await Lotterymodel.findOne({ dateToday: formattedDate });
+    try {
+        console.log("here is the formatted date ")
 
-    // if (findDOC) {
-    //     // Update the document with the set number
-    //     const updatedDoc = await Lotterymodel.findOneAndUpdate(
-    //         { dateToday: formattedDate },
-    //         { $set: obj },
-    //         { new: true }
-    //     );
-    //     console.log("Updated Lottery Document:", updatedDoc);
-    // } else {
-    //     // Create a new document for the day
-    //     obj["dateToday"] = formattedDate;
-    //     obj["day"] = day;
-    //     const createDoc = await Lotterymodel.create(obj);
-    //     console.log("Created Lottery Document:", createDoc);
-    // }
+        // Find the Lottery Document by date
+        const findDOC = await Lotterymodel.findOne({ dateToday: formattedDate });
 
-    // Fetch the bet results for the specified number and type
-    const { winners, losers } = await getBetResults(type, number, current);
+        if (findDOC) {
+          // Update the document with the set number
+          const updatedDoc = await Lotterymodel.findOneAndUpdate(
+            { dateToday: formattedDate },
+            { $set: obj },
+            { new: true }
+          );
+          console.log("Updated Lottery Document:", updatedDoc);
+        } else {
+          // Create a new document for the day
+          obj["dateToday"] = formattedDate;
+          obj["day"] = day;
+          const createDoc = await Lotterymodel.create(obj);
+          console.log("Created Lottery Document:", createDoc);
+        }
 
-    console.log("Winners:", winners);
-    console.log("Losers:", losers);
+        // Fetch the bet results for the specified number and type
+        const { winners, losers } = await getBetResults(type, number, current);
 
-    // Update bet results in the clients collection
-    await updateBetResults(winners, losers);
+        console.log("Winners:", winners);
+        console.log("Losers:", losers);
 
-    res.send({ message: "Betting number set and results updated." });
-  } catch (error) {
-    console.log("Error:", error);
-    res.status(500).send(error);
-  }
+        // Update bet results in the clients collection
+        await updateBetResults(winners, losers);
+        let querytype;
+        if (type === "firstNumber") {
+            querytype = "single";
+        } else if (type === "secondNumber") {
+            querytype = "jodi";
+        } else if (type === "thirdNumber") {
+            querytype = "teen";
+        }
+
+
+
+        // here we are deleting the bets
+        // const findAnddeleteuser = await lotteryBetModel.find({ betType: querytype })
+        // for (let user of findAnddeleteuser) {
+        //     console.log(user, "hello world")
+        //     await lotteryBetModel.findByIdAndDelete(user._id)
+        // }
+        // console.log(findAnddelete)
+
+        res.send({ message: "Betting number set and results updated." });
+    } catch (error) {
+        console.log("Error:", error);
+        res.status(500).send(error);
+    }
 }
 
 
 module.exports.result = async (req, res) => {
-  console.log("hello world")
-  try {
-    const foundDoc = await lotteryModel.findOne({})
-    console.log(foundDoc)
-    res.send(foundDoc)
-  } catch (error) {
-    console.log(error)
-    res.send(error)
-  }
+    console.log("hello world")
+    try {
+        const foundDoc = await lotteryModel.findOne({})
+        console.log(foundDoc)
+        res.send(foundDoc)
+    } catch (error) {
+        console.log(error)
+        res.send(error)
+    }
 }
 
 
 
 module.exports.GettheLotteryNumber = async (req, res) => {
-  // const {date} = req.body
-  console.log("hello")
-  console.log("from getthe number")
-  try {
-    const respo = await Lotterymodel.find({})
-    console.log(respo)
-    res.send(respo)
-  } catch (error) {
-    console.log(error)
+    // const {date} = req.body
+    console.log("hello")
+    console.log("from getthe number")
+    try {
+        const respo = await Lotterymodel.find({})
+        console.log(respo)
+        res.send(respo)
+    } catch (error) {
+        console.log(error)
 
-  }
-  // res.send("frm get the number")
+    }
+    // res.send("frm get the number")
 }
 
 
 
 module.exports.ResetNumber = async (req, res) => {
-  console.log("from reset")
-  let year = moment()
-  // let year =  year.format("dd DD")
+    console.log("from reset")
+    let year = moment()
+    // let year =  year.format("dd DD")
 
-  let dare = year.format("dd DD")
-  console.log(dare)
+    let dare = year.format("dd DD")
+    console.log(dare)
 
 
-  res.send()
+    res.send()
 }
--- app/website/lottery/routes/index.js
+++ app/website/lottery/routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("/createLotteryBet", lotteryController.createBet)//creates bet 
 router.post("/readbytype",lotteryController.readBetsfromType)//kind of... it gives stats to admin 
 router.post("/LotterysetNumber",lotteryController.SetBettingNumber)//setting number from admin side 
-router.get("/LotteryResult",lotteryController.result) 
+// router.get("/LotteryResult",lotteryController.result) 
 router.get("/LotteryGetTheNUmber",lotteryController.GettheLotteryNumber)//to get the data of the current day 
 router.get("/ResetLotteryNUmbers",lotteryController.ResetNumber)//dummy api for this project 
 
